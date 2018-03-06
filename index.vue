7ab72106a032f0ecf21520b56ee374e9fa649469
Shivam Singh <devatquarxss@gmail.com>
2018-03-06 06:27:41 +0530

    v1

---
+++ index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    index
+
+  </div>
+</template>
+
+<script>
+  import {mapMutations} from 'vuex'
+  import {mapGetters} from 'vuex'
+  //
+  export default {
+    //
+    methods:{
+      ...mapMutations([
+
+      ])
+    },
+    computed:{
+      ...mapGetters([
+
+      ])
+    },
+    created(){
+
+    }
+    //
+  }
+</script>
+
